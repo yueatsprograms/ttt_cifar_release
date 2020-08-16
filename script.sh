acac817fb7615850d19a8f8e79930240c9afe8b5
Yu Sun <yueatsprograms@gmail.com>
2020-08-16 10:34:31 -0700

    fix gn flag

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 CUDA_VISIBLE_DEVICES=0 python main.py --shared layer2 --rotation_type expand \
+			--group_norm 8 \
 			--nepoch 150 --milestone_1 75 --milestone_2 125 \
 			--outf results/cifar10_layer2_gn_expand
 
